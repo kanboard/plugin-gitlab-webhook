fd174c46ea744151e0fc74fa76cd1c9fd76b5e90
Frederic Guillot <fred@kanboard.net>
2016-02-10 20:34:24 -0500

    Remove useless slash in icon url

--- Template/project/integrations.php
+++ Template/project/integrations.php
@@ -1,4 +1,4 @@
-<h3><img src="<?= $this->url->dir() ?>/plugins/GitlabWebhook/gitlab-icon.png"/>&nbsp;<?= t('Gitlab webhooks') ?></h3>
+<h3><img src="<?= $this->url->dir() ?>plugins/GitlabWebhook/gitlab-icon.png"/>&nbsp;<?= t('Gitlab webhooks') ?></h3>
 <div class="listing">
 <input type="text" class="auto-select" readonly="readonly" value="<?= $this->url->href('webhook', 'handler', array('plugin' => 'GitlabWebhook', 'token' => $webhook_token, 'project_id' => $project['id']), false, '', true) ?>"/><br/>
 <p class="form-help"><a href="http://kanboard.net/plugins/gitlab-webhook" target="_blank"><?= t('Help on Gitlab webhooks') ?></a></p>
