18ee3f32fdbdf68ee2cd6f05b6e95ab79b5e4cd5
Frederic Guillot <fred@kanboard.net>
2016-05-13 23:00:48 -0400

    Replace Gitlab logo image by Font Awesome icon

--- Template/project/integrations.php
+++ Template/project/integrations.php
@@ -1,4 +1,4 @@
-<h3><img src="<?= $this->url->dir() ?>plugins/GitlabWebhook/gitlab-icon.png"/>&nbsp;<?= t('Gitlab webhooks') ?></h3>
+<h3><i class="fa fa-gitlab fa-fw" aria-hidden="true"></i><?= t('Gitlab webhooks') ?></h3>
 <div class="listing">
 <input type="text" class="auto-select" readonly="readonly" value="<?= $this->url->href('webhook', 'handler', array('plugin' => 'GitlabWebhook', 'token' => $webhook_token, 'project_id' => $project['id']), false, '', true) ?>"/><br/>
 <p class="form-help"><a href="https://kanboard.net/plugin/gitlab-webhook" target="_blank"><?= t('Help on Gitlab webhooks') ?></a></p>
